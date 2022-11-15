5e94ca06a30a3e41b398e72b6ec9be50743c2929
Pfumiko <50150564+OlegSolomatin@users.noreply.github.com>
2022-11-15 16:35:33 +0300

    Revert "Quiz feature"

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,6 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {
-      screens: {
-        'mob': { 'raw': '(max-width: 767.7px)' },
-        // => @media (min-height: 800px) { ... }
-      }
-    },
   darkMode: "class",
   theme: {
     extend: {},
